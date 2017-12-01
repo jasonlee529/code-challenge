8df33be148b7ea15d3c5ceae63612ced96a5b35a
jasonlee529 <libo529@gmail.com>
2017-12-01 11:42:02 +0800

    update package

--- src/main/java/cn/lee/hackerrank/algrithms/sorting/tutorial/insertionsort/InsertionSortPart2.java
+++ src/main/java/cn/lee/hackerrank/algrithms/sorting/tutorial/insertionsort/InsertionSortPart2.java
@@ -1,4 +1,4 @@
-package cn.lee.hackerrank.algrithms.sorting.tutorial;
+package cn.lee.hackerrank.algrithms.sorting.tutorial.insertionsort;
 
 /**
  * <pre>
---
+++ src/main/java/cn/lee/hackerrank/algrithms/sorting/tutorial/quicksort/Quicksort1Partition.java
@@ -0,0 +1,7 @@
+package cn.lee.hackerrank.algrithms.sorting.tutorial.quicksort;
+
+/**
+ * Created by jason on 17-12-1.
+ */
+public class Quicksort1Partition {
+}
