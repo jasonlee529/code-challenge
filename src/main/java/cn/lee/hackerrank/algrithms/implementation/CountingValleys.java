1c4f02b16d3dc7f54d2828ef37851dd5468fb8f5
jasonlee529 <libo529@gmail.com>
2017-12-05 22:34:45 +0800

    complete counting vallerys

---
+++ src/main/java/cn/lee/hackerrank/algrithms/implementation/CountingValleys.java
@@ -0,0 +1,69 @@
+package cn.lee.hackerrank.algrithms.implementation;
+
+import java.util.Scanner;
+
+/**
+ * <pre>
+ *     Gary is an avid hiker. He tracks his hikes meticulously, paying close attention to small details like topography. During his last hike, he took exactly  steps. For every step he took, he noted if it was an uphill or a downhill step. Gary's hikes start and end at sea level. We define the following terms:
+ *
+ * A mountain is a non-empty sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+ * A valley is a non-empty sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+ * Given Gary's sequence of up and down steps during his last hike, find and print the number of valleys he walked through.
+ *
+ * Input Format
+ *
+ * The first line contains an integer, , denoting the number of steps in Gary's hike.
+ * The second line contains a single string of  characters. Each character is  (where U indicates a step up and D indicates a step down), and the  character in the string describes Gary's  step during the hike.
+ *
+ * Constraints
+ *
+ * Output Format
+ *
+ * Print a single integer denoting the number of valleys Gary walked through during his hike.
+ *
+ * Sample Input
+ *
+ * 8
+ * UDDDUDUU
+ * Sample Output
+ *
+ * 1
+ * Explanation
+ *
+ * If we represent _ as sea level, a step up as /, and a step down as \, Gary's hike can be drawn as:
+ *
+ * _/\      _
+ * \    /
+ * \/\/
+ * It's clear that there is only one valley there, so we print  on a new line.
+ * </pre>
+ * Created by jason on 17-12-5.
+ */
+public class CountingValleys {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String steps = in.next();
+        int result = steps(n, steps);
+        System.out.println(result);
+    }
+
+    private static int steps(int n, String steps) {
+        int r = 0;
+        int t = 0;
+        int v = 0;
+        for (int i = 0; i < steps.length(); i++) {
+            int m = steps.charAt(i) == 'U' ? 1 : -1;
+            t += m;
+            if (t < 0) {
+                v = 1;
+            }
+            if (t >= 0 && v == 1) {
+                r++;
+                v = 0;
+            }
+        }
+        return r;
+    }
+}
