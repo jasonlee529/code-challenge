5e4b45d56971ec3354882aabb653a3d7e7249531
jasonlee529 <libo529@gmail.com>
2017-12-06 11:40:52 +0800

    complete ElectronicsShop

--- src/main/java/cn/lee/hackerrank/algrithms/implementation/ElectronicsShop.java
+++ src/main/java/cn/lee/hackerrank/algrithms/implementation/ElectronicsShop.java
@@ -1,6 +1,9 @@
 package cn.lee.hackerrank.algrithms.implementation;
 
+import java.util.Scanner;
+
 /**
+ * https://www.hackerrank.com/challenges/electronics-shop/problem
  * <pre>
  *
  *     Monica wants to buy exactly one keyboard and one USB drive from her favorite electronics store. The store sells  different brands of keyboards and  different brands of USB drives. Monica has exactly  dollars to spend, and she wants to spend as much of it as possible (i.e., the total cost of her purchase must be maximal).
@@ -49,4 +52,36 @@ package cn.lee.hackerrank.algrithms.implementation;
  * Created by jason on 17-12-5.
  */
 public class ElectronicsShop {
+    static int getMoneySpent(int[] keyboards, int[] drives, int s) {
+        // Complete this function
+        int max = -1;
+        for (int k : keyboards) {
+            for (int d : drives) {
+                int p = k + d;
+                if (p > s) {
+                    continue;
+                }
+                max = p > max ? p : max;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] keyboards = new int[n];
+        for (int keyboards_i = 0; keyboards_i < n; keyboards_i++) {
+            keyboards[keyboards_i] = in.nextInt();
+        }
+        int[] drives = new int[m];
+        for (int drives_i = 0; drives_i < m; drives_i++) {
+            drives[drives_i] = in.nextInt();
+        }
+        //  The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+        int moneySpent = getMoneySpent(keyboards, drives, s);
+        System.out.println(moneySpent);
+    }
 }
