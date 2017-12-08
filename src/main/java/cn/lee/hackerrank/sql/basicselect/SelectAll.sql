75bbc3158e982b20eb82fc608ae6a75187118723
jasonlee529 <libo529@gmail.com>
2017-12-08 11:20:41 +0800

    complete sql 2

---
+++ src/main/java/cn/lee/hackerrank/sql/basicselect/SelectAll.sql
@@ -0,0 +1,12 @@
+/*/*
+  https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+
+Query all columns (attributes) for every row in the CITY table.
+
+Input Format
+
+The CITY table is described as follows:
+
+*/
+
+select * from CITY
