c911c88fcdfacfa4709488cd3d70108aced2a113
jasonlee529 <libo529@gmail.com>
2017-12-12 11:51:15 +0800

    complete binary tree sort 1

--- src/main/java/cn/lee/hackerrank/sql/advancedselect/BinarySearchTree1.sql
+++ src/main/java/cn/lee/hackerrank/sql/advancedselect/BinarySearchTree1.sql
@@ -2,3 +2,15 @@
 https://www.hackerrank.com/challenges/binary-search-tree-1/problem
  */
 
+SELECT
+  DISTINCT t1.n,
+  CASE
+  WHEN t1.p IS NULL THEN
+    'Root'
+  when n in (select distinct p from bst) then 'Inner'
+  else 'Leaf' end p
+
+FROM
+  bst t1
+
+order by t1.n
