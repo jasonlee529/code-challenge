7a034da7ed82c96775f8b1cd75e205ea45d49715
jasonlee529 <libo529@gmail.com>
2017-12-23 20:52:13 +0800

    update

---
+++ src/main/java/cn/lee/hackerrank/tutorials/code30days/Day_22_Binary_Search_Trees/Node.java
@@ -0,0 +1,14 @@
+package cn.lee.hackerrank.tutorials.code30days.Day_22_Binary_Search_Trees;
+
+/**
+ * Created by jason on 17-12-23.
+ */
+class Node {
+    Node left, right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
