96220daf7f8378a75673148dc8c5f56a96208cdd
jasonlee529 <libo529@gmail.com>
2017-12-25 11:40:26 +0800

    complate

---
+++ src/main/java/cn/lee/hackerrank/tutorials/code30days/Day24_More_Linked_Lists/Node.java
@@ -0,0 +1,14 @@
+package cn.lee.hackerrank.tutorials.code30days.Day24_More_Linked_Lists;
+
+/**
+ * Created by jason on 17-12-25.
+ */
+public class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
