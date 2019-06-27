0be7992b99eee35b8a0384620cc19b7ab590fa7f
jason <libo529@gmail.com>
2019-06-27 19:20:17 +0800

    updte question

---
+++ src/main/java/cn/lee/lintcode/cat/Question3.java
@@ -0,0 +1,22 @@
+package cn.lee.lintcode.cat;
+
+/**
+ * https://www.lintcode.com/problem/a-b-problem/description?_from=cat
+ */
+public class Question1 {
+
+    public int aplusb(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        int sum = a ^ b;
+        int carry = (a & b) << 1;
+        return aplusb(sum, carry);
+    }
+
+    public static void main(String[] args) {
+        Question1 question = new Question1();
+        System.out.println(question.aplusb(1, 2));
+        System.out.println(question.aplusb(1, -2));
+    }
+}
