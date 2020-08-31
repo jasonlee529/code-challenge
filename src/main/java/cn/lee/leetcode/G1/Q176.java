96983d983e0dccf46a695fe196ac3684544e2ac6
libo <libo@infisan.cn>
2020-08-31 16:28:07 +0800

    G1

---
+++ src/main/java/cn/lee/leetcode/G1/Q176.java
@@ -0,0 +1,14 @@
+package cn.lee.leetcode.G1;import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**  
+    * @Title: Q176
+    * @Description: https://leetcode-cn.com/problems/second-highest-salary/submissions/
+    * @author libo
+    * @date 2020/8/31 16:17
+    * @Version 1.0
+    */
+@Slf4j
+public class Q176 {
+//    SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT n,1
+}
