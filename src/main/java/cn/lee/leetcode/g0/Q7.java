47fe47ed3d992501abfd59cadf7bc32a5cf309a6
jason <libo529@gmail.com>
2020-09-03 22:49:58 +0800

    add files

---
+++ src/main/java/cn/lee/leetcode/g0/Q7.java
@@ -0,0 +1,34 @@
+package cn.lee.leetcode.g0;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Title: Q7
+ * @Description: https://leetcode-cn.com/problems/reverse-integer/
+ * @author: libo
+ * @date: 2020/9/3 22:16
+ * @Version: 1.0
+ */
+@Slf4j
+public class Q7 {
+    public static void main(String[] args) {
+        Q7 q7 = new Q7();
+        System.out.println(q7.reverse(-100));
+    }
+
+    public int reverse(int x) {
+        String s = String.valueOf(x);
+        StringBuilder sb = new StringBuilder();
+        if(s.charAt(0)=='+'||s.charAt(0)=='-'){
+            sb.append(s.charAt(0));
+            for(int i=s.length()-1;i>=1;i--){
+                sb.append(s.charAt(i));
+            }
+        }else{
+            for(int i=s.length()-1;i>=0;i--){
+                sb.append(s.charAt(i));
+            }
+        }
+        return Integer.valueOf(sb.toString());
+    }
+}
