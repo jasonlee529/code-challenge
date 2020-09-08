20e0067eb2057c30be1617ef9f8f654d39a4e0db
jason <libo529@gmail.com>
2020-09-08 18:14:05 +0800

    Q83

---
+++ src/main/java/cn/lee/leetcode/g0/Q83.java
@@ -0,0 +1,37 @@
+package cn.lee.leetcode.g0;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Title: Q83
+ * @Description: https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+ * @author: libo
+ * @date: 2020/9/8 18:07
+ * @Version: 1.0
+ */
+@Slf4j
+public class Q83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head==null?null : head.next;
+        ListNode pre = head;
+        while (cur != null) {
+            if(cur.val==pre.val){
+                pre.next=cur.next;
+                cur=cur.next;
+            }else{
+                pre=cur;
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
