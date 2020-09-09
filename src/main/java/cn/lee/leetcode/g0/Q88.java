3316001d57ea0e3f4bf382e58f8f697ef5da406e
jason <libo529@gmail.com>
2020-09-09 16:13:01 +0800

    update

--- src/main/java/cn/lee/leetcode/g0/Q88.java
+++ src/main/java/cn/lee/leetcode/g0/Q88.java
@@ -17,19 +17,41 @@ public class Q88 {
         System.out.println(Arrays.toString(new Q88().merge(new int[]{}, 3, new int[]{}, 2)));
     }
 
+    /**
+     * 复制排序
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     * @return
+     */
     public int[] merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i=0;i<n;i++){
-            nums1[i+m]=nums2[i];
+        for (int i = 0; i < n; i++) {
+            nums1[i + m] = nums2[i];
         }
-        for(int i=0;i<m+n;i++){
-            for (int j=i+1;j<m+n;j++){
-                if(nums1[i]>nums1[j]){
-                    int t= nums1[i];
-                    nums1[i]=nums1[j];
-                    nums1[j]=t;
+        for (int i = 0; i < m + n; i++) {
+            for (int j = i + 1; j < m + n; j++) {
+                if (nums1[i] > nums1[j]) {
+                    int t = nums1[i];
+                    nums1[i] = nums1[j];
+                    nums1[j] = t;
                 }
             }
         }
         return nums1;
+    }
+
+    /**
+     * 尾插
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     * @return
+     */
+    public int[] merge2(int[] nums1, int m, int[] nums2, int n) {
+        return null;
     }
 }
