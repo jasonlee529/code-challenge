8f8446e8e2560a84ecaed5a3c24103a116234e9d
jason <libo529@gmail.com>
2020-09-09 14:01:55 +0800

    Q88

---
+++ src/main/java/cn/lee/leetcode/g0/Q88.java
@@ -0,0 +1,35 @@
+package cn.lee.leetcode.g0;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+
+/**
+ * @Title: Q88
+ * @Description: https://leetcode-cn.com/problems/merge-sorted-array/
+ * @author: libo
+ * @date: 2020/9/9 13:50
+ * @Version: 1.0
+ */
+@Slf4j
+public class Q88 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Q88().merge(new int[]{}, 3, new int[]{}, 2)));
+    }
+
+    public int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i=0;i<n;i++){
+            nums1[i+m]=nums2[i];
+        }
+        for(int i=0;i<m+n;i++){
+            for (int j=i+1;j<m+n;j++){
+                if(nums1[i]>nums1[j]){
+                    int t= nums1[i];
+                    nums1[i]=nums1[j];
+                    nums1[j]=t;
+                }
+            }
+        }
+        return nums1;
+    }
+}
