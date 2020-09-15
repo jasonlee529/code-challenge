77006e854373893a47a66fe8cf70effd629f662a
jason <libo529@gmail.com>
2020-09-15 11:43:16 +0800

    Q155

---
+++ src/main/java/cn/lee/leetcode/util/ListNode.java
@@ -0,0 +1,20 @@
+package cn.lee.leetcode.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Title: ListNode
+ * @Description:
+ * @author: libo
+ * @date: 2020/9/15 11:31
+ * @Version: 1.0
+ */
+@Slf4j
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
