9f382cb5a2890a7a72c384f47164ef3f012b48ec
jason <libo529@gmail.com>
2020-09-23 16:18:58 +0800

    Q1539

---
+++ src/main/java/cn/lee/leetcode/probolems/g15/Q1539.java
@@ -0,0 +1,37 @@
+package cn.lee.leetcode.probolems.g15;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Title: Q1539
+ * @Description: https://leetcode-cn.com/problems/kth-missing-positive-number/
+ * @author: libo
+ * @date: 2020/9/23 16:05
+ * @Version: 1.0
+ */
+@Slf4j
+public class Q1539 {
+    public static void main(String[] args) {
+        System.out.println(new Q1539().findKthPositive(new int[]{2, 3, 4, 7, 11}, 1));
+        System.out.println(new Q1539().findKthPositive(new int[]{2, 3, 4, 7, 11}, 2));
+        System.out.println(new Q1539().findKthPositive(new int[]{2, 3, 4, 7, 11}, 3));
+        System.out.println(new Q1539().findKthPositive(new int[]{2, 3, 4, 7, 11}, 4));
+        System.out.println(new Q1539().findKthPositive(new int[]{2, 3, 4, 7, 11}, 5));
+        System.out.println(new Q1539().findKthPositive(new int[]{1,2, 3, 4, 7, 11}, 2));
+    }
+
+    public int findKthPositive(int[] arr, int k) {
+        int cnt = 0;
+        int left = 0;
+        int cur = 1;
+        while (cnt < k) {
+            if (left < arr.length && arr[left] == cur) {
+                left++;
+            } else {
+                cnt++;
+            }
+            cur++;
+        }
+        return cur-1;
+    }
+}
