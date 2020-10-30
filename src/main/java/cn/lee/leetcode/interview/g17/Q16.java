de748d74b7cac9ae39de23ed7ee09c3ae65aa03e
jason <libo529@gmail.com>
2020-10-30 16:31:57 +0800

    Q746

--- src/main/java/cn/lee/leetcode/interview/g17/Q16.java
+++ src/main/java/cn/lee/leetcode/interview/g17/Q16.java
@@ -25,6 +25,7 @@ public class Q16 {
 
     public static void main(String[] args) {
         System.out.println(new Q16().massage(new int[]{2, 1, 1, 2}));
+        System.out.println(new Q16().massage(new int[]{1, 2, 3, 4}));
     }
 
     public int massage(int[] nums) {
@@ -35,11 +36,15 @@ public class Q16 {
         int dp0 = 0, dp1 = nums[0];
 
         for (int i = 1; i < n; ++i) {
+            System.out.print(i + " " + nums[i] + " pre: " + dp0 + " " + dp1);
+
             int tdp0 = Math.max(dp0, dp1); // 计算 dp[i][0]
             int tdp1 = dp0 + nums[i]; // 计算 dp[i][1]
 
             dp0 = tdp0; // 用 dp[i][0] 更新 dp_0
             dp1 = tdp1; // 用 dp[i][1] 更新 dp_1
+            System.out.println(" after: " + dp0 + " " + dp1);
+
         }
         return Math.max(dp0, dp1);
     }
@@ -50,5 +55,22 @@ public class Q16 {
         }
         int pre = Math.max(f(nums, i - 2) + nums[i], f(nums, i - 1));
         return pre;
+    }
+
+    public int massage3(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[length - 1];
     }
 }
