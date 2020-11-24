e729de59baeb39b62cd5f41128d48abafc508ac0
jason <libo529@gmail.com>
2020-11-24 10:53:00 +0800

    Q414

--- src/main/java/cn/lee/leetcode/probolems/g4/Q414.java
+++ src/main/java/cn/lee/leetcode/probolems/g4/Q414.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.*;
+
 /**
  * @Title: Q414
  * @Description: https://leetcode-cn.com/problems/third-maximum-number/
@@ -16,9 +18,48 @@ public class Q414 {
     @Test
     public void test() {
         Assert.assertEquals(1, thirdMax(new int[]{2, 2, 3, 1}));
+        Assert.assertEquals(-2147483648, thirdMax(new int[]{1, 2, -2147483648}));
+    }
+
+    @Test
+    public void test2() {
+        Set<Integer> sets = new TreeSet<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+        sets.add(1);
+        sets.add(-1);
+        sets.add(Integer.MIN_VALUE);
+        System.out.println(Arrays.toString(sets.toArray(new Integer[]{})));
     }
 
     public int thirdMax(int[] nums) {
+        Set<Integer> sets = new TreeSet<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 == o1 ? 0 : o2 > o1 ? 1 : -1;
+            }
+        });
+        for (int n : nums) {
+            sets.add(n);
+        }
+        Iterator<Integer> iterator = sets.iterator();
+        int first = iterator.next();
+        int index = 1;
+        while (iterator.hasNext()) {
+            index++;
+            if (index == 3) {
+                first = iterator.next();
+                break;
+            }
+            iterator.next();
+        }
+        return first;
+    }
+
+    public int thirdMax2(int[] nums) {
         int first = Integer.MIN_VALUE;
         int second = Integer.MIN_VALUE;
         int thrid = Integer.MIN_VALUE;
