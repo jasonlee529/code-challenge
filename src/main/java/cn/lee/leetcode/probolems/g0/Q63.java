48729bb3399e65af0760a57e1e93a65cea660818
jason <libo529@gmail.com>
2020-12-23 10:28:28 +0800

    Q62

--- src/main/java/cn/lee/leetcode/probolems/g0/Q63.java
+++ src/main/java/cn/lee/leetcode/probolems/g0/Q63.java
@@ -16,17 +16,50 @@ public class Q63 {
 
     @Test
     public void test1() {
-        Assert.assertEquals(2, new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}});
+        Assert.assertEquals(2, uniquePathsWithObstacles(new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
     }
 
     @Test
     public void test2() {
-        Assert.assertEquals(1, new int[][]{{0, 1}, {0, 0,}});
+        Assert.assertEquals(1, uniquePathsWithObstacles(new int[][]{{0, 1}, {0, 0}}));
     }
 
+    @Test
+    public void test3() {
+        Assert.assertEquals(1, uniquePathsWithObstacles(new int[][]{{0}}));
+    }
+
+    @Test
+    public void test4() {
+        Assert.assertEquals(0, uniquePathsWithObstacles(new int[][]{{1}}));
+    }
+    @Test
+    public void test5() {
+        Assert.assertEquals(0, uniquePathsWithObstacles(new int[][]{{1},{0}}));
+    }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-
-
-        return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] pos = new int[m][n];
+        pos[m - 1][0] = 1;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[m - i - 1][j] == 1) {
+                    pos[i][j] = 0;
+                    continue;
+                }
+                if (i == (m - 1) && j == 0) {
+                    continue;
+                }
+                if (j == 0) {
+                    pos[i][j] = pos[i + 1][j];
+                } else if (i == (m - 1)) {
+                    pos[i][j] = pos[i][j - 1];
+                } else {
+                    pos[i][j] = pos[i + 1][j] + pos[i][j - 1];
+                }
+            }
+        }
+        return pos[0][n - 1];
     }
 }
