f56b55fdc04654791b8ed0962ac75344493a9128
jason <libo529@gmail.com>
2021-01-05 09:30:16 +0800

    Q188

---
+++ src/main/java/cn/lee/leetcode/probolems/g8/Q830.java
@@ -0,0 +1,15 @@
+package cn.lee.leetcode.probolems.g8;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**  
+    * @Title: Q830
+    * @Description: 
+    * @author: libo
+    * @date: 2021/1/5 9:30
+    * @Version: 1.0
+    */
+@Slf4j
+public class Q830 {
+}
