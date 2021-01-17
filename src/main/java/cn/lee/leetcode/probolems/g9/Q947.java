deb9770c802732219f8638f4ed694702e685d8ca
jason <libo529@gmail.com>
2021-01-17 13:01:00 +0800

    Q1232

---
+++ src/main/java/cn/lee/leetcode/probolems/g9/Q947.java
@@ -0,0 +1,26 @@
+package cn.lee.leetcode.probolems.g9;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @Title: Q947
+ * @Description: https://leetcode-cn.com/problems/most-stones-removed-with-same-row-or-column/
+ * @author: libo
+ * @date: 2021/1/15 13:49
+ * @Version: 1.0
+ */
+@Slf4j
+public class Q947 {
+    @Test
+    public void test1() {
+        Assert.assertEquals(5, removeStones(new int[][]{{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}}));
+    }
+
+    public int removeStones(int[][] stones) {
+        
+
+        return 0;
+    }
+}
