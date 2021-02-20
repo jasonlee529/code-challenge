8c46e96fa1d36dbb337c8a745dc08baf57eab328
jason <libo529@gmail.com>
2021-02-20 09:41:19 +0800

    Q1004

---
+++ src/main/java/cn/lee/leetcode/probolems/g6/Q697.java
@@ -0,0 +1,19 @@
+package cn.lee.leetcode.probolems.g6;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Title: Q697
+ * @Description: https://leetcode-cn.com/problems/degree-of-an-array/
+ * @author: libo
+ * @date: 2021/2/20 9:05
+ * @Version: 1.0
+ */
+@Slf4j
+public class Q697 {
+
+	public int findShortestSubArray(int[] nums) {
+
+		return 0;
+	}
+}
