2c31f041ab9ab53836315c32be4e3e11a11015db
jason <libo529@gmail.com>
2021-09-24 10:15:17 +0800

    Q430

---
+++ src/main/java/cn/lee/leetcode/util/Node3Factory.java
@@ -0,0 +1,20 @@
+package cn.lee.leetcode.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Title: Node3Factory
+ * @Description:
+ * @author: libo
+ * @date: 2021/9/24 9:56
+ * @Version: 1.0
+ */
+@Slf4j
+public class Node3Factory {
+	public static Node3 build(Integer[] arr) {
+
+		return null;
+	}
+}
