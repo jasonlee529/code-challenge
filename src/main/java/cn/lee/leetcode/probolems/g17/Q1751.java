8eec86931464492243ada58491efeba3712eab57
jason <libo529@gmail.com>
2022-02-16 11:54:11 +0800

    Q1751

--- src/main/java/cn/lee/leetcode/probolems/g17/Q1751.java
+++ src/main/java/cn/lee/leetcode/probolems/g17/Q1751.java
@@ -1,5 +1,6 @@
 package cn.lee.leetcode.probolems.g17;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Stack;
 
@@ -21,10 +22,52 @@ public class Q1751 {
 
 	public int maxValue(int[][] events, int k) {
 		int res = 0;
-		PriorityQueue<Integer> queue = new PriorityQueue<>();
+		PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2 - o1;
+			}
+		});
 		Stack<int[]> path = new Stack<>();
+		dfs(events, 0, k, queue, path);
+		return queue.size() == 0 ? 0 : queue.peek();
+	}
 
-		return res;
+	private void dfs(int[][] events, int begin, int k, PriorityQueue<Integer> queue, Stack<int[]> path) {
+		if (path.size() == k || begin == events.length) {
+			queue.offer(sum(path));
+			return;
+		}
+		for (int i = begin; i < events.length; i++) {
+			int[] e = events[i];
+			boolean can = canJoin(path, e);
+			if (can) {
+				path.push(e);
+			}
+			dfs(events, i + 1, k, queue, path);
+			if (can) {
+				path.pop();
+			}
+		}
+	}
+
+	private boolean canJoin(Stack<int[]> path, int[] e2) {
+		boolean can = true;
+		for (int[] e : path) {
+			if (!(e[0] > e2[1] || e[1] < e2[0])) {
+				can = false;
+				break;
+			}
+		}
+		return can;
+	}
+
+	private Integer sum(Stack<int[]> path) {
+		int sum = 0;
+		for (int[] e : path) {
+			sum += e[2];
+		}
+		return sum;
 	}
 
 	@Test
@@ -41,4 +84,10 @@ public class Q1751 {
 	public void test_3() {
 		Assert.assertEquals(9, maxValue(new int[][]{{1, 1, 1}, {2, 2, 2}, {3, 3, 3}, {4, 4, 4}}, 3));
 	}
+
+	@Test
+	public void test_4() {
+		Assert.assertEquals(9, maxValue(new int[][]{{1, 3, 4}, {2, 4, 1}, {1, 1, 4}, {3, 5, 1}, {2, 5, 5}}, 3));
+	}
+
 }
