595673ccf1d35a67b71445d94a2c85ea6077f48c
jason <libo529@gmail.com>
2022-02-18 07:04:00 +0800

    Q66

---
+++ src/main/java/cn/lee/leetcode/offer/Q66.java
@@ -0,0 +1,31 @@
+package cn.lee.leetcode.offer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Title: Q66
+ * @Description: https://leetcode-cn.com/problems/gou-jian-cheng-ji-shu-zu-lcof/
+ * @author: libo
+ * @date: 2022/2/18 0018 6:58
+ * @Version: 1.0
+ */
+@Slf4j
+public class Q66 {
+
+    public int[] constructArr(int[] a) {
+        int[] b = new int[a.length];
+
+
+        return b;
+    }
+
+    @Test
+    public void test_1() {
+        Assert.assertArrayEquals(new int[]{120, 60, 40, 30, 24}, constructArr(new int[]{1, 2, 3, 4, 5}));
+    }
+
+}
